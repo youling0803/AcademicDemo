71485edcf9e14e0d1889cd73be4194cbadaea30d
Xuke <Xuke@DESKTOP-536VODM>
2020-05-11 22:22:48 +0800

    测试提交

--- Academic_demo/Academic_demo/Global.asax.cs
+++ Academic_demo/Academic_demo/Global.asax.cs
@@ -16,6 +16,7 @@ namespace Academic_demo
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //test1111
         }
     }
 }
