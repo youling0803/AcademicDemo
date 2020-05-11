8822c55adf547830e42a990f8b810e951d6d81d8
youling0803 <1352862898@qq.com>
2020-05-11 20:22:57 +0800

    ///

---
+++ Academic_demo/Academic_demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Academic_demo.Startup))]
+namespace Academic_demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
